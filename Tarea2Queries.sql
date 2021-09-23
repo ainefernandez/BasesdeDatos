f197f063c608bd6a8bd24c947ae8275285e76c5a
ainefernandez <90108813+ainefernandez@users.noreply.github.com>
2021-09-23 14:13:36 -0500

    Update Tarea2Queries.sql

--- Tarea2Queries.sql
+++ Tarea2Queries.sql
@@ -1,4 +1,4 @@
-#Tarea2 Queries: Primer intento
+--Tarea2 Queries: Primer intento
 
 --Obtener un reporte de edades de los empleados para checar su elegibilidad para seguro de gastos médicos menores.
 select first_name, last_name, age(now(),birth_date)
